eddeab47c0649674deda2c2b764dd7c24b191d7f
naoto-kamba <ayataka.nk@gmail.com>
2021-04-08 18:26:04 +0900

    tailwindcssの導入

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,22 +1,7 @@
-import { theme } from '../theme'
-
 export const Header = () => {
   return (
-    <div className="root">
-      <div className="label">Tea</div>
-      <style jsx>{`
-        .root {
-          display: flex;
-          align-items: center;
-          height: 56px;
-          background-color: ${theme.color.primary};
-        }
-        .label {
-          color: ${theme.font.inverted};
-          padding: 10px;
-          font-size: 1.5rem;
-        }
-      `}</style>
+    <div className="flex items-center p-2 bg-red-900 h-12">
+      <div className="text-2xl text-white">Tea</div>
     </div>
   )
 }
