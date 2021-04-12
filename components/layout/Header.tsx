cc8bdb41ad8e76d05bd474cd222333cd9f9bb3ee
naoto-kamba <ayataka.nk@gmail.com>
2021-04-12 19:10:17 +0900

    タイトルクリックでトップページに遷移させる

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,7 +1,12 @@
 export const Header = () => {
+  const onClickTop = () => {
+    location.assign('/')
+  }
   return (
     <div className="flex items-center p-2 h-12 border-b">
-      <div className="text-2xl text-black">Tea-Calc</div>
+      <div className="text-2xl text-black cursor-pointer" onClick={onClickTop}>
+        Tea-Calc
+      </div>
     </div>
   )
 }
